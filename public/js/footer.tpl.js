cf7fdca9780e1e355125bc6dcd9c5c8b506a2e49
Joshua Koudys <jkoudys@gmail.com>
2020-01-18 21:02:36 -0500

    feat: footer has links

--- public/js/footer.tpl.js
+++ public/js/footer.tpl.js
@@ -1,10 +1,11 @@
 (() => {
+  const email = 'inquiries@cobaltcounsel.com';
+  const address = '330 Bay Street, Suite 1400 Toronto, Ontario, Canada M5H2S8';
   const el = document.createElement('footer');
   el.id = 'colophon';
   el.className = 'site-footer';
   el.setAttribute('role', 'contentinifo');
   el.innerHTML = `
-<footer id="colophon" class="site-footer" role="contentinfo">
   <div class="container">
     <div class="col-sm-4"></div>
     <div class="col-sm-4">
@@ -24,13 +25,12 @@
     <div class="site-info">
       <div class="col-sm-10 col-sm-offset-1 section-line section-line-footer"></div>
       <div class="site-info-inner">
-        Main (Mailing Address): 330 Bay Street, Suite 1400 Toronto, Ontario, Canada M5H2S8
-        Telephone: 1-647-749-0047 Fax: 1-866-678-7731 E-mail: inquiries@cobaltcounsel.com |
-        Copyright © 2017 Cobalt Lawyers. All Rights Reserved.
+        Main (Mailing Address): <a target="_blank" href="https://www.google.com/maps/place/${address}">${address}</a>
+        Telephone: 1-647-749-0047 Fax: 1-866-678-7731 E-mail: <a href="${email}">${email}</a> |
+        Copyright © ${new Date().getFullYear()} Cobalt Lawyers. All Rights Reserved.
       </div>
     </div>
   </div>
-</footer>
     `;
 
   document.currentScript.parentNode.insertBefore(el, document.currentScript);
