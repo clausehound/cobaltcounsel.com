21e06c76b44240a8f2525191edfc7c400165ef2d
Laura Pomeroy <laura@clausehound.com>
2023-05-04 12:49:43 -0400

    feat: simplifies javascript function for the show more toggle

--- public/js/readMore.js
+++ public/js/readMore.js
@@ -1,13 +1,7 @@
-var acc = document.getElementsByClassName("readMoreToggle");
-var i;
-
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    /* Toggle between adding and removing the "active" class,
-    to highlight the button that controls the panel */
+for (const toggleEl of document.getElementsByClassName("readMoreToggle")) 
+  { toggleEl.addEventListener("click", function() {
     this.classList.toggle("active");
 
-    /* Toggle between hiding and showing the active panel */
     var panel = this.nextElementSibling;
     if (panel.style.display === "block") {
       panel.style.display = "none";
