6d858679a018b95f0eb4391bc936d2ad7e81b997
gajeev <gajeev@gmail.com>
2024-11-22 11:09:41 -0500

    adds dispute page

---
+++ src/components/molecules/dispute/disputeSetup.ts
@@ -0,0 +1,46 @@
+import { createElement as h } from 'react';
+import styled from 'styled-components';
+import Container from '@atoms/container';
+import ContentBlock from '@atoms/contentBlock';
+
+const DisputeSetup = () => {
+  return h(
+    Container,
+    {
+      content: true,
+      contentTop: true,
+    },
+    h(
+      ContentBlock,
+      null,
+      h(
+        IntroBlock,
+        null,
+        'When you are having trouble collecting on invoices issued to your customers, are dealing with scope creep issues or unfulfilled scope, or otherwise have heartburn from one one your customers, vendors, employees, contractors or shareholders, consult with us.',
+      ),
+      h(
+        IntroBlock,
+        null,
+        'Understanding what the law says on the topic can be simplified with our AI research approach.  Answers are then refined by our lawyers who will walk you through your various options.',
+      ),
+      h(
+        IntroBlock,
+        null,
+        'We may be able to assist you with the drafting of a letter to help resolve your dispute within an hour.',
+      ),
+    ),
+  );
+};
+
+export default DisputeSetup;
+
+const Intro = styled.div`
+  h2 {
+    margin-bottom: 0;
+  }
+  text-align: center;
+`;
+
+const IntroBlock = styled.h3`
+  line-height: 1.25;
+`;
---
+++ src/pages/dispute.ts
@@ -0,0 +1,95 @@
+import { createElement as h } from 'react';
+import styled from 'styled-components';
+import Layout from '@utils/layout';
+import SEO from '@utils/seo';
+import Button from '@atoms/button';
+import Container from '@atoms/container';
+import DisputeHeader from '@molecules/dispute/disputeHeader';
+import DisputeSetup from '@molecules/dispute/disputeSetup';
+import SectionCheckout from '@molecules/sectionCheckout';
+
+const DisputePage = () => {
+  return h(Layout, {
+    showFullNav: false,
+    children: [
+      h(SEO, { title: 'Commercial Dispute Resolution by Cobalt Lawyers' }),
+      h(DisputeHeader),
+      h(DisputeSetup),
+      h(SectionCheckout, { checkoutCopy: h('h1', null, 'Book a call to discuss') }),
+    ],
+  });
+};
+
+export default DisputePage;
+
+const Image = styled.div``;
+
+const CaseStudyButton = styled(Button)`
+  display: block;
+  width: 19rem;
+`;
+
+const ContentContainer = styled(Container)`
+  padding-bottom: 1em;
+  &:after,
+  &:before {
+    background: inherit;
+    content: '';
+    display: block;
+    height: 50%;
+    left: 0;
+    position: absolute;
+    right: 0;
+    transform: skewY(-4.5deg);
+    transform-origin: 100%;
+    z-index: -1;
+  }
+  &:after {
+    bottom: 0;
+    border-bottom: 1px solid ${(p) => p.theme.colors.black};
+  }
+  &:before {
+    top: 0;
+  }
+  &:nth-of-type(even) {
+    direction: rtl;
+    border-top: 1px solid ${(p) => p.theme.colors.black};
+    background-color: ${(props) => props.theme.colors.secondary};
+    div {
+      direction: ltr;
+    }
+  }
+`;
+
+const CaseStudyContainer = styled(Container)`
+  margin-top: 5rem;
+  margin-bottom: 2rem;
+  padding-bottom: 1em;
+  &:after,
+  &:before {
+    background: inherit;
+    content: '';
+    display: block;
+    height: 50%;
+    left: 0;
+    position: absolute;
+    right: 0;
+    transform-origin: 100%;
+    z-index: -1;
+  }
+  &:after {
+    bottom: 0;
+    border-bottom: 1px solid ${(p) => p.theme.colors.black};
+  }
+  &:before {
+    top: 0;
+  }
+  &:nth-of-type(even) {
+    direction: rtl;
+    border-top: 1px solid ${(p) => p.theme.colors.black};
+    background-color: ${(props) => props.theme.colors.secondary};
+    div {
+      direction: ltr;
+    }
+  }
+`;
