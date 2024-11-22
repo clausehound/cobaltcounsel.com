0e841b6262a17c0ff989dac9dc52a785980e462a
gajeev <gajeev@gmail.com>
2024-11-22 11:57:12 -0500

    Revert "adds sitemap plugin"
    This reverts commit 301ab98112364c03b0570882e6aa11888e22cf7e.

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,7 +11,6 @@ module.exports = {
     `gatsby-plugin-sharp`,
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-anchor-links`,
-    `gatsby-plugin-sitemap`,
     {
       resolve: "gatsby-plugin-react-svg",
       options: {
