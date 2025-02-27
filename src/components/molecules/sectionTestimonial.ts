9e8ce7e2bc38abe42ecb555f7b5b59db7401463a
gajeev <gajeev@gmail.com>
2025-02-27 11:12:50 -0500

    adds see more button to landing page

--- src/components/molecules/sectionTestimonial.ts
+++ src/components/molecules/sectionTestimonial.ts
@@ -1,10 +1,11 @@
-import { createElement as h } from "react";
-import styled from "styled-components";
-import Container from "@atoms/container";
-import { media } from "@utils/media";
+import { createElement as h } from 'react';
+import styled from 'styled-components';
+import Container from '@atoms/container';
+import { media } from '@utils/media';
 
-import SeperatorOne from "@assets/seperators/a-seperator-1.svg";
-import SeperatorThree from "@assets/seperators/a-seperator-3.svg";
+import SeperatorOne from '@assets/seperators/a-seperator-1.svg';
+import SeperatorThree from '@assets/seperators/a-seperator-3.svg';
+import Button from '@atoms/button';
 
 const SectionTestimonial = () => {
   return h(
@@ -12,12 +13,9 @@ const SectionTestimonial = () => {
     {
       content: true,
       contentTop: true,
+      textCenter: true,
     },
-    h(
-      Intro,
-      null,
-      h("h2", null, "Testimonials")
-    ),
+    h(Intro, null, h('h2', null, 'Testimonials')),
     h(
       Setup,
       null,
@@ -26,56 +24,64 @@ const SectionTestimonial = () => {
         null,
         h(Seperator, null, h(SeperatorOne, null)),
         h(
-          "p",
+          'p',
           null,
-          "We have been impressed with the commitment by Cobalt to providing a thoughtful response within a six hour service level, with their deal knowledge capture through their software platform, and with the speed at which their software and lawyers are able to collect our thinking and map from prior deals into future deals. As our understanding of their legal and technological capabilities have advanced, we have increased our desire to create [...] automations around legal documentation."
+          'We have been impressed with the commitment by Cobalt to providing a thoughtful response within a six hour service level, with their deal knowledge capture through their software platform, and with the speed at which their software and lawyers are able to collect our thinking and map from prior deals into future deals. As our understanding of their legal and technological capabilities have advanced, we have increased our desire to create [...] automations around legal documentation.',
         ),
         h(
-          "p",
+          'p',
           null,
-          h("strong", null, "— Michael Younder"),
-          " | ",
-          h("em", null, "Head of CMS Partnerships")
-        )
+          h('strong', null, '— Michael Younder'),
+          ' | ',
+          h('em', null, 'Head of CMS Partnerships'),
+        ),
       ),
       h(
         Step,
         null,
         h(Seperator, null, h(SeperatorThree, null)),
         h(
-          "p",
+          'p',
           null,
-          "Cobalt has been an excellent partner for us. The client focus and support is outstanding. Their Drafting Sprints have been very effective for us, helping progress deals based on our timeline. It’s easy to recommend Cobalt."
+          'Cobalt has been an excellent partner for us. The client focus and support is outstanding. Their Drafting Sprints have been very effective for us, helping progress deals based on our timeline. It’s easy to recommend Cobalt.',
         ),
-        h(
-          "p",
-          null,
-          h("strong", null, "— Brett Hurlock"),
-          " | ",
-          h("em", null, "President")
-        )
+        h('p', null, h('strong', null, '— Brett Hurlock'), ' | ', h('em', null, 'President')),
       ),
       h(
         Step,
         null,
         h(
-          "p",
+          'p',
           null,
-          "The protection and collection of personal information of the audience that use our services are of great importance to us, and with the depth of experience of the Cobalt team, they have helped us to ensure that we understand how we can ensure our compliance with privacy laws and provide our audience comfort that their information is secure. The Cobalt team shares the same values of transparency and trust as is evident in our experiences working with them."
+          'The protection and collection of personal information of the audience that use our services are of great importance to us, and with the depth of experience of the Cobalt team, they have helped us to ensure that we understand how we can ensure our compliance with privacy laws and provide our audience comfort that their information is secure. The Cobalt team shares the same values of transparency and trust as is evident in our experiences working with them.',
         ),
-        h(
-          "p",
-          null,
-          h("strong", null, "— Brian Prosserman"),
-          " | ",
-          h("em", null, "Founder")
-        )
-      )
-    )
+        h('p', null, h('strong', null, '— Brian Prosserman'), ' | ', h('em', null, 'Founder')),
+      ),
+    ),
+    h(
+      SeeMoreButtonContainer,
+      null,
+      h(
+        Button,
+        {
+          to: '/testimonials',
+        },
+        'See More Testimonials',
+      ),
+    ),
   );
 };
 
 export default SectionTestimonial;
+
+const SeeMoreButtonContainer = styled.div`
+  width: 100%;
+  text-align: center;
+  margin-top: 2rem;
+  @media ${media.md} {
+    margin-top: 0;
+  }
+`;
 
 const Intro = styled.div`
   margin-top: 6rem;
@@ -144,7 +150,7 @@ const Work = styled.div`
   }
 
   &:after {
-    content: "";
+    content: '';
     position: absolute;
     width: calc(100% + 0.8rem);
     left: -0.4rem;
