b4c874c96adaa1a446f6718bf8cc147cef1683e5
sudo-install-isaiah <isaiah.iu@hotmail.com>
2026-01-20 14:51:10 -0500

    Create temp page for learn/policy

--- src/pages/learn/policy.ts
+++ src/pages/learn/policy.ts
@@ -1,1 +1,6 @@
 //TODO: Placeholder for learning policy page
+import { createElement as h } from 'react';
+
+export default function PolicyPage() {
+  return h('div', null, 'Policy Learning Page Coming Soon');
+}
